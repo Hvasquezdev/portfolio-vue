d5b1329f214472bf4718e4963b209a0b12cd51ba
Hector Vasquez <hvasquezdev@gmail.com>
2019-03-22 21:15:59 -0400

    Fix router 404 problem when refresh page

--- src/router.js
+++ src/router.js
@@ -6,7 +6,6 @@ const Certifications = () => import ('@/views/Certifications.vue');
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
